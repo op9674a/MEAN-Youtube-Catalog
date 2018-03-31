f1b291911d4467d3de4cb68763af03025c74dc23
Olena Penn <olenapenn@gmail.com>
2018-03-30 16:00:21 -1000

    added controllers routes

--- controllers/videos.js
+++ controllers/videos.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const videos = express.Router();
+const router = express.Router();
 const Video = require('../models/videos.js');
 
 // Index
-videos.get('/',  (req,res) => {
+router.get('/',  (req,res) => {
 	res.json(videos);
 });
 
+//Create
+// router.post("/", (req, res)=>{
+//     Videos.create(req.body, (err, createdVideo)=> {
+//         res.json(createdVideo);
+//     });
+// });
 
-module.exports = videos;
+//Delete
+// router.delete("/:id", (req, res)=>{
+//     Videos.findByIdAndRemove(req.params.id, (err, deletedVideo) => {
+//         res.json(deletedAnimal)
+//     });
+// });
+
+//Edit
+// router.put("/:id", (req, res) => {
+//     Videos.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedVideo)=>{
+//         res.json(updatedVideo);
+//     })
+// })
+
+
+module.exports = router;
